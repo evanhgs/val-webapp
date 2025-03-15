708b7e0a7d03ee8bab92397ae4284aeb660b4b9d
evan <evan@mail.com>
2025-03-15 13:11:27 +0000

    📝: update production documentation to clarify server setup and remove wsgi.py

--- server/wsgi.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-
-# force le repertoire Valenstagram à etre dans le python path pour le mode production
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
-from server.app import app
-
-if __name__ == "__main__":
-    app.run()
