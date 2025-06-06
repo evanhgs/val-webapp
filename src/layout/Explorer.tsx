447fe67b5ead879d0f474ac124e7b00eb779bad9
evan <evanhugues@proton.me>
2025-06-06 15:06:08 +0200

    💄 : add navigation button to return to previous page in Explorer layout

--- src/layout/Explorer.tsx
+++ src/layout/Explorer.tsx
@@ -39,7 +39,19 @@ const Explorer = () => {
     return (
         <>
             {error && <div className="error-message">{error}</div>}
-            <NavPosts post={globalFeed?.userFeed || []} />
+
+            <div className="px-[20%]">
+                <div className="my-6 pl-2 ">
+                    <button onClick={() => navigate(-1)} className="inline-flex items-center text-white bg-gray-800 hover:bg-gray-700 rounded-full px-4 py-2 transition duration-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                        </svg>
+                        Retour
+                    </button>
+                </div>
+                <NavPosts post={globalFeed?.userFeed || []} />
+            </div>
+
         </>
     );
 }
