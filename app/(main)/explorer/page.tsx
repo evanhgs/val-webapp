013c361a7a26f2a87f1d4fc2b0081297dbc1c925
evan <evanhugues@proton.me>
2025-11-28 20:55:45 +0100

    feat: enhance and modernize UI styling for Upload and Explorer pages, improving layout consistency and design alignment with min-h-screen and updated button styles

--- app/(main)/explorer/page.tsx
+++ app/(main)/explorer/page.tsx
@@ -41,9 +41,9 @@ export default function Explorer(){
         <>
             {error && <div className="error-message">{error}</div>}
 
-            <div className="px-[20%]">
+            <div className="px-[20%] flex min-h-screen">
                 <div className="my-6 pl-2 ">
-                    <button onClick={() => navigate.back()} className="inline-flex items-center text-white bg-gray-800 hover:bg-gray-700 rounded-full px-4 py-2 transition duration-200">
+                    <button onClick={() => navigate.back()} className="inline-flex items-center bg-zinc-900 hover:bg-zinc-500 hover:text-zinc-900 rounded-xl px-4 py-2 transition duration-200">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                         </svg>
